cea4ae8f6a934f79a62907d97628f5be3237aa5c
saefulbarkah <barkahgaming00102@gmail.com>
2021-08-11 10:23:54 +0700

    fix: Error Handling

--- base-laravel/saeful barkah/laravel2/resources/views/errors/404.blade.php
+++ base-laravel/saeful barkah/laravel2/resources/views/errors/404.blade.php
@@ -4,6 +4,6 @@
 @section('content')
 <h1>404</h1>
 <h2>Halaman tidak di temukan</h2>
-<p>Maaf, halaman yang Anda coba lihat tidak ada.</p>
+<p>Maaf, halaman yang Anda coba lihat tidak ada</p>
 <a href="{{ route('home') }}" class="btn btn-main mt-20">Kembali</a>
 @endsection
