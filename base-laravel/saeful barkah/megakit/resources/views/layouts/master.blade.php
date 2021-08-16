b4719efb67bacf5864d8ba27f33049335cdbf437
saefulbarkah <barkahgaming00102@gmail.com>
2021-08-16 09:36:26 +0700

    [MEGAKIT] memindahkan baris code css dan js ke master

--- base-laravel/saeful barkah/megakit/resources/views/layouts/master.blade.php
+++ base-laravel/saeful barkah/megakit/resources/views/layouts/master.blade.php
@@ -1,11 +1,27 @@
 <!doctype html>
 <html lang="en">
 
-    {{-- head --}}
-    <head>
-        @include('layouts.head')
-    </head>
-    {{-- end head --}}
+{{-- head --}}
+
+<head>
+    <title> @yield('title')</title>
+
+    <!-- bootstrap.min css -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}">
+    <!-- Icon Font Css -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/themify/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/all.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/magnific-popup/dist/magnific-popup.css') }}">
+    <!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/slick-carousel/slick/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/slick-carousel/slick/slick-theme.css') }}">
+
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+</head>
+
+{{-- end head --}}
 
 <body>
 
@@ -26,7 +42,21 @@
     {{-- end content --}}
 
     {{-- javascripts --}}
-    @include('layouts.js')
+
+    <script src="{{ asset('assets/plugins/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('assets/js/contact.js') }}"></script>
+    <!-- Bootstrap 4.3.1 -->
+    <script src="{{ asset('assets/plugins/bootstrap/js/popper.js') }}"></script>
+    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+    <!--  Magnific Popup-->
+    <script src="{{ asset('assets/plugins/magnific-popup/dist/jquery.magnific-popup.min.js') }}"></script>
+    <!-- Slick Slider -->
+    <script src="{{ asset('assets/plugins/slick-carousel/slick/slick.min.js') }}"></script>
+    <!-- Counterup -->
+    <script src="{{ asset('assets/plugins/counterup/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/counterup/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('assets/js/script.js') }}"></script>
+
     {{-- end javascripts --}}
 </body>
 
