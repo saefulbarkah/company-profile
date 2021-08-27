3e7b9bd60e5288df206f2a86d75f34e39e30a9bc
saefulbarkah <51852948+saefulbarkah@users.noreply.github.com>
2021-08-27 13:30:02 +0700

    [woodrox][navbar] menambahkan menu layanan

--- base-laravel/saeful barkah/woodrox/resources/views/layouts/navbar.blade.php
+++ base-laravel/saeful barkah/woodrox/resources/views/layouts/navbar.blade.php
@@ -14,21 +14,7 @@
                  <ul class="nav navbar-nav menu_nav ml-auto">
                      <li class="nav-item active"><a class="nav-link" href="{{ url('/') }}">Home</a></li>
                      <li class="nav-item"><a class="nav-link" href="{{ url('/about') }}">About</a></li>
-                     <li class="nav-item submenu dropdown">
-                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
-                         <ul class="dropdown-menu">
-                             <li class="nav-item"><a class="nav-link" href="projects.html">Project</a></li>
-                             <li class="nav-item"><a class="nav-link" href="project-details.html">Project Details</a></li>
-                             <li class="nav-item"><a class="nav-link" href="elements.html">Elements</a></li>
-                         </ul>
-                     </li>
-                     <li class="nav-item submenu dropdown">
-                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
-                         <ul class="dropdown-menu">
-                             <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
-                             <li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>
-                         </ul>
-                     </li>
+                     <li class="nav-item"><a class="nav-link" href="{{ url('/layanan') }}">Layanan</a></li>
                      <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
                  </ul>
              </div>
