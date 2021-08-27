c2142bb4a14dbe540d81c016084ecd37785d3db3
saefulbarkah <51852948+saefulbarkah@users.noreply.github.com>
2021-08-27 10:54:25 +0700

    [bingo][gmap] menghapus plugin map

--- base-laravel/saeful barkah/bingo/resources/views/_layouts/master.blade.php
+++ base-laravel/saeful barkah/bingo/resources/views/_layouts/master.blade.php
@@ -75,9 +75,6 @@
     =====================================-->
     <!-- Main jQuery -->
     <script src="{{ asset('_template/plugins/jquery/jquery.min.js') }}"></script>
-    <!-- Google Map -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu5nZKbeK-WHQ70oqOWo-_4VmwOwKP9YQ"></script>
-    <script src="{{ asset('_template/plugins/google-map/gmap.js') }}"></script>
 
     <!-- Form Validation -->
     <script src="{{ asset('_template/plugins/form-validation/jquery.form.js') }}"></script>
@@ -98,7 +95,6 @@
 
     <!-- Custom js -->
     <script src="{{ asset('_template/js/script.js') }}"></script>
-    {{-- end footer --}}
 
 </body>
 
