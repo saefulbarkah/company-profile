029b4e84563e7734b5397c5d1042ea11c818b03a
saefulbarkah <51852948+saefulbarkah@users.noreply.github.com>
2021-08-27 13:46:19 +0700

    [woodrox][layanan] membuat halaman layanan

--- base-laravel/saeful barkah/woodrox/resources/views/service/index.blade.php
+++ base-laravel/saeful barkah/woodrox/resources/views/service/index.blade.php
@@ -3,71 +3,70 @@
 
 @section('title', 'Genetic | Layanan')
 @section('content')
-    <section class="page-title bg-1">
+<!--================Home Banner Area =================-->
+<section class="banner_area">
+    <div class="banner_inner d-flex align-items-center">
+        <div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
         <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="block text-center">
-                        <span class="text-white">Layanan kami</span>
-                        <h1 class="text-capitalize mb-4 text-lg">Apa yang kita lakukan</h1>
-                        <ul class="list-inline">
-                            <li class="list-inline-item"><a href="index.html" class="text-white">Home</a></li>
-                            <li class="list-inline-item"><span class="text-white">/</span></li>
-                            <li class="list-inline-item"><a href="#" class="text-white-50">layanan kami</a></li>
-                        </ul>
-                    </div>
+            <div class="banner_content text-center">
+                <h2>Layanan</h2>
+                <div class="page_link">
+                    <a href="index.html">Beranda</a>
+                    <a href="about-us.html">Layanan</a>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
+<!--================End Home Banner Area =================-->
 
-    <!--  Section Services Start -->
-    <section class="section service border-top">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-7 text-center">
-                    <div class="section-title">
-                        <span class="h6 text-color">Pelayanan kami</span>
-                        <h2 class="mt-3 content-title ">Kami menyediakan berbagai layanan kreatif</h2>
-                    </div>
+<!--================Feature Area =================-->
+<section class="feature_area p_120">
+    <div class="container">
+        <div class="main_title">
+            <h2>Some Features that Made us Unique</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+        </div>
+        <div class="row feature_inner">
+            <div class="col-lg-4 col-md-6">
+                <div class="feature_item">
+                    <h4><i class="lnr lnr-user"></i>Expert Technicians</h4>
+                    <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power.</p>
                 </div>
             </div>
-
-            <div class="row justify-content-center">
-                <div class="col-lg-6 col-md-6 col-sm-6">
-                    <div class="service-item mb-5">
-                        <i class="ti-desktop"></i>
-                        <h4 class="mb-3">Pengembangan website.</h4>
-                        <p>Agensi digital tidak hadir untuk menggantikan tim internal Anda, kami di sini untuk bermitra</p>
-                    </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="feature_item">
+                    <h4><i class="lnr lnr-license"></i>Professional Service</h4>
+                    <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power.</p>
                 </div>
-
-                <div class="col-lg-6 col-md-6 col-sm-6">
-                    <div class="service-item mb-5">
-                        <i class="ti-layers"></i>
-                        <h4 class="mb-3">Desain antarmuka.</h4>
-                        <p>Agensi digital tidak hadir untuk menggantikan tim internal Anda, kami di sini untuk bermitra</p>
-                    </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="feature_item">
+                    <h4><i class="lnr lnr-phone"></i>Great Support</h4>
+                    <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="feature_item">
+                    <h4><i class="lnr lnr-rocket"></i>Technical Skills</h4>
+                    <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="feature_item">
+                    <h4><i class="lnr lnr-diamond"></i>Highly Recomended</h4>
+                    <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="feature_item">
+                    <h4><i class="lnr lnr-bubble"></i>Positive Reviews</h4>
+                    <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power.</p>
                 </div>
             </div>
         </div>
-    </section>
-    <!--  Section Services End -->
+    </div>
+</section>
+<!--================End Feature Area =================-->
 
-    <section class="cta-2">
-        <div class="container">
-            <div class="cta-block p-5 rounded">
-                <div class="row justify-content-center align-items-center ">
-                    <div class="col-lg-7">
-                        <span class="text-color">Untuk Setiap jenis bisnis</span>
-                        <h2 class="mt-2 text-white">Percayakan Proyek Anda kepada Tim Profesional Terbaik Kami</h2>
-                    </div>
-                    <div class="col-lg-4">
-                        <a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&to=saefulbarkah650@gmailcom&su=Saya%20berminat!&body=Halo"
-                            class="btn btn-main btn-round-full float-right">Hubungi kami</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
 @endsection
