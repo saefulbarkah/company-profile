00c07661dd8e7383d42dd760a8fb1625622482de
saefulbarkah <51852948+saefulbarkah@users.noreply.github.com>
2021-08-28 13:13:20 +0700

    [GP - about] membuat halaman about

--- base-laravel/saeful barkah/gp/resources/views/layouts/navbar.blade.php
+++ base-laravel/saeful barkah/gp/resources/views/layouts/navbar.blade.php
@@ -1,17 +1,17 @@
 <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center justify-content-lg-between">
 
-      <h1 class="logo me-auto me-lg-0"><a href="{{ url('/') }}">Genetic<span> Wear</span></a></h1>
-      <nav id="navbar" class="navbar order-last order-lg-0">
-        <ul>
-          <li><a class="nav-link scrollto active" href="{{ url('/') }}">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav>
+        <h1 class="logo me-auto me-lg-0"><a href="{{ url('/') }}">Genetic<span> Wear</span></a></h1>
+        <nav id="navbar" class="navbar order-last order-lg-0">
+            <ul>
+                <li><a class="nav-link scrollto active" href="{{ url('/') }}">Home</a></li>
+                <li><a class="nav-link scrollto" href="{{ url('/about') }}">About</a></li>
+                <li><a class="nav-link scrollto" href="#services">Services</a></li>
+                <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+                <li><a class="nav-link scrollto" href="#team">Team</a></li>
+                <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
     </div>
-  </header>
+</header>
--- base-laravel/saeful barkah/gp/routes/web.php
+++ base-laravel/saeful barkah/gp/routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/','HomeController@home');
+Route::get('/about','AboutController@aboutUs');
