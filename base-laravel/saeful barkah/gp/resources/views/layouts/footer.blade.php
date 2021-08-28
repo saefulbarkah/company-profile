c367b3264ab50bf9aa9a28559b557c34285947d3
saefulbarkah <51852948+saefulbarkah@users.noreply.github.com>
2021-08-28 13:56:24 +0700

    [GP - footer] menambah infromasi footer

--- base-laravel/saeful barkah/gp/resources/views/layouts/footer.blade.php
+++ base-laravel/saeful barkah/gp/resources/views/layouts/footer.blade.php
@@ -3,14 +3,13 @@
         <div class="container">
             <div class="row">
 
-                <div class="col-lg-3 col-md-6">
+                <div class="col-lg-4">
                     <div class="footer-info">
-                        <h3>Gp<span>.</span></h3>
+                        <h3>Genetic <span>Wear</span></h3>
                         <p>
-                            A108 Adam Street <br>
-                            NY 535022, USA<br><br>
-                            <strong>Phone:</strong> +1 5589 55488 55<br>
-                            <strong>Email:</strong> info@example.com<br>
+                            Jl. Katapang Andir No.503, Sangkanhurip, Kec. Katapang, Bandung, Jawa Barat 40921<br><br>
+                            <strong>Telepon:</strong> +62 81221337876<br>
+                            <strong>Email:</strong> genetic.wear@gmail.com<br>
                         </p>
                         <div class="social-links mt-3">
                             <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
@@ -21,20 +20,8 @@
                         </div>
                     </div>
                 </div>
-
-                <div class="col-lg-2 col-md-6 footer-links">
-                    <h4>Useful Links</h4>
-                    <ul>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-lg-3 col-md-6 footer-links">
-                    <h4>Our Services</h4>
+                <div class="col-lg-4 footer-links">
+                    <h4>Layanan Kamis</h4>
                     <ul>
                         <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
                         <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
@@ -42,6 +29,13 @@
                         <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
                         <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
                     </ul>
+                </div>
+                <div class="col-lg-4 footer-links">
+                    <h4>Lokasi</h4>
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.071232505725!2d107.5719010142015!3d-7.000893970512589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68eead56b8f89f%3A0x3fc9157f74855daf!2sGenetic%20Wear%20Workshop!5e0!3m2!1sid!2sid!4v1630132691439!5m2!1sid!2sid"
+                        width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy">
+                    </iframe>
                 </div>
             </div>
         </div>
