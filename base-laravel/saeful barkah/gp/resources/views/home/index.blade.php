16e3d9600920e21d895de3a4d68037a633e00006
saefulbarkah <barkahgaming00102@gmail.com>
2021-08-31 14:59:14 -0700

    [GP - beranda] menambahkan animasi pada timeline

--- base-laravel/saeful barkah/gp/resources/views/home/index.blade.php
+++ base-laravel/saeful barkah/gp/resources/views/home/index.blade.php
@@ -79,22 +79,30 @@
         </div>
         <div class="row mt-4">
             <ul class="timeline">
+
+                {{-- 1 --}}
                 <li class="tml-1">
                     <div class="timeline-image"><img class="rounded-circle img-fluid"
                             src="{{  asset('assets/img/time-line/1.jpg') }}" alt="..." /></div>
                     <div class="timeline-panel">
-                        <div class="timeline-heading">
-                            <h4 class="subheading">Our Humble Beginnings</h4>
-                        </div>
-                        <div class="timeline-body">
-                            <p class="">Bisa desain kostum dan mock up kostum</p>
+                        <div class="fade-right" data-aos="fade-right">
+                            <div class="timeline-heading">
+                                <h4 class="subheading">Our Humble Beginnings</h4>
+                            </div>
+                            <div class="timeline-body">
+                                <p class="">Bisa desain kostum dan mock up kostum</p>
+                            </div>
                         </div>
                     </div>
                 </li>
+
+                {{-- 2 --}}
                 <li class="timeline-inverted tml-2">
-                    <div class="timeline-image"><img class="rounded-circle img-fluid"
-                            src="{{  asset('assets/img/time-line/2.jpg') }}" alt="..." /></div>
-                    <div class="timeline-panel">
+                    <div class="timeline-image">
+                        <img class="rounded-circle img-fluid" src="{{  asset('assets/img/time-line/2.jpg') }}"
+                            alt="..." />
+                    </div>
+                    <div class="timeline-panel" data-aos="fade-left">
                         <div class="timeline-heading">
                             <h4 class="subheading">An Agency is Born</h4>
                         </div>
@@ -105,24 +113,28 @@
                         </div>
                     </div>
                 </li>
+
+                {{-- 3 --}}
                 <li class="tml-3">
                     <div class="timeline-image"><img class="rounded-circle img-fluid"
                             src="{{  asset('assets/img/time-line/3.jpg') }}" alt="..." /></div>
                     <div class="timeline-panel">
-                        <div class="timeline-heading">
-                            <h4 class="subheading">Transition to Full Service</h4>
-                        </div>
-                        <div class="timeline-body">
-                            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut
-                                voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero
-                                unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                        <div class="fade-right" data-aos="fade-right">
+                            <div class="timeline-heading">
+                                <h4 class="subheading">Our Humble Beginnings</h4>
+                            </div>
+                            <div class="timeline-body">
+                                <p class="">Bisa desain kostum dan mock up kostum</p>
+                            </div>
                         </div>
                     </div>
                 </li>
+
+                {{-- 4 --}}
                 <li class="timeline-inverted tml-4">
                     <div class="timeline-image"><img class="rounded-circle img-fluid"
                             src="{{  asset('assets/img/time-line/4.jpg') }}" alt="..." /></div>
-                    <div class="timeline-panel">
+                    <div class="timeline-panel" data-aos="fade-left">
                         <div class="timeline-heading">
                             <h4 class="subheading">Phase Two Expansion</h4>
                         </div>
@@ -134,7 +146,7 @@
                     </div>
                 </li>
                 <li class="timeline-inverted tml-5">
-                    <div class="timeline-image">
+                    <div class="timeline-image" data-aos="fade-top">
                         <h4>
                             Be Part
                             <br />
