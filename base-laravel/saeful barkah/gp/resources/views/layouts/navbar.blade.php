29249bb5cc2a3ec394f920b036057bb5bb20bb8c
saefulbarkah <barkahgaming00102@gmail.com>
2021-08-31 15:36:15 -0700

    [GP - navbar] menambahkan aktif navbar di setiap halaman

--- base-laravel/saeful barkah/gp/resources/views/layouts/navbar.blade.php
+++ base-laravel/saeful barkah/gp/resources/views/layouts/navbar.blade.php
@@ -5,11 +5,25 @@
         </h1>
         <nav id="navbar" class="navbar order-last order-lg-0">
             <ul>
-                <li><a class="nav-link scrollto active" href="{{ url('/') }}">Home</a></li>
-                <li><a class="nav-link scrollto" href="{{ url('/about') }}">Tentang</a></li>
-                <li><a class="nav-link scrollto" href="{{ url('service') }}">Layanan</a></li>
-                <li><a class="nav-link scrollto " href="{{ url('portfolio') }}">Portofolio</a></li>
-                <li><a class="nav-link scrollto" href="{{ url('contact') }}">kontak</a></li>
+                <li>
+                    <a class="nav-link scrollto {{ Request::is('/') ? 'active' : '' }}" href="{{ url('/') }}">Home</a>
+                </li>
+                <li>
+                    <a class="nav-link scrollto {{ Request::is('about') ? 'active' : '' }}"
+                        href="{{ url('/about') }}">Tentang</a>
+                </li>
+                <li>
+                    <a class="nav-link scrollto {{ Request::is('service') ? 'active' : '' }}"
+                        href="{{ url('service') }}">Layanan</a>
+                </li>
+                <li>
+                    <a class="nav-link scrollto {{ Request::is('portfolio') ? 'active' : '' }}"
+                        href="{{ url('portfolio') }}">Portofolio</a>
+                </li>
+                <li>
+                    <a class="nav-link scrollto {{ Request::is('contact') ? 'active' : '' }}"
+                        href="{{ url('contact') }}">kontak</a>
+                </li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav>
