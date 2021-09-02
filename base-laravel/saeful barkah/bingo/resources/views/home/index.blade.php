7396c8018b97774674f933a4bba3729c87857c52
saefulbarkah <barkahgaming00102@gmail.com>
2021-09-02 08:29:07 +0700

    [Bingo - home] menghapus section

--- base-laravel/saeful barkah/bingo/resources/views/home/index.blade.php
+++ base-laravel/saeful barkah/bingo/resources/views/home/index.blade.php
@@ -110,48 +110,6 @@
 
 
 
-<!--Start About Section-->
-<section class="about-2 section" id="about">
-    <div class="container">
-        <div class="row">
-
-            <!-- section title -->
-            <div class="col-12">
-                <div class="title text-center">
-                    <h2>Kami Adalah Agen Bingo</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam reprehenderit accusamus
-                        labore iusto,
-                        aut, eum itaque illo totam tempora eius.</p>
-                    <div class="border"></div>
-                </div>
-            </div>
-            <!-- /section title -->
-
-            <div class="col-md-6">
-                <img src="{{ asset('assets/images/about/about-2.png') }}" class="img-fluid" alt="">
-            </div>
-            <div class="col-md-6">
-                <ul class="checklist">
-                    <li>Donec sed odio dui. Aenean eu leo quam. Pellentesque ornare sem laca quam venenatis vestibulum.
-                    </li>
-                    <li>Aenean quam. Pellentesque ornare sem laca quam venenatis vestibulum.</li>
-                    <li>Donec sed odio dui. Aenean eu leo quam. Pellentesque ornare sem laca quam venenatis vestibulum.
-                    </li>
-                    <li>Etiam porta sem multipage evint landing magna mollis euismod a pharetra augue.</li>
-                    <li>Aenean quam. Pellentesque ornare sem laca quam venenatis vestibulum.</li>
-                </ul>
-                <a href="#" class="btn btn-main mt-20">Pelajari lebih lanjut</a>
-            </div>
-        </div>
-        <!-- End row -->
-    </div>
-    <!-- End container -->
-</section>
-<!-- End section -->
-
-
-
-
 <!-- Start Call To Action-->
 <section class="call-to-action section">
     <div class="container">
@@ -168,66 +126,6 @@
     <!-- End container -->
 </section>
 <!-- End section -->
-
-
-
-
-<!-- Start Counter Section -->
-<section class="counter-wrapper section-sm">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <div class="title">
-                    <h2>Agensi Pemenang Penghargaan</h2>
-                    <p>Vestibulum nisl tortor, consectetur quis imperdiet bibendum, laoreet sed arcu. Sed condimentum
-                        iaculis ex, in faucibus lorem accumsan non. Donec mattis tincidunt metus. Morbi sed tortor a
-                        risus luctus dignissim.</p>
-                </div>
-            </div>
-            <!-- first count item -->
-            <div class="col-md-4 col-sm-6 col-xs-6 text-center ">
-                <div class="counters-item">
-                    <i class="tf-ion-ios-alarm-outline"></i>
-                    <div>
-                        <span class="counter" data-count="150">0</span>
-                    </div>
-                    <h3>Klien senang</h3>
-                </div>
-            </div>
-            <!-- end first count item -->
-
-            <!-- second count item -->
-            <div class="col-md-4 col-sm-6 col-xs-6 text-center ">
-                <div class="counters-item">
-                    <i class="tf-ion-ios-analytics-outline"></i>
-                    <div>
-                        <span class="counter" data-count="130">0</span>
-                    </div>
-                    <h3>Proyek selesai</h3>
-                </div>
-            </div>
-            <!-- end second count item -->
-
-            <!-- third count item -->
-            <div class="col-md-4 col-sm-6 col-xs-6 text-center ">
-                <div class="counters-item">
-                    <i class="tf-ion-ios-compose-outline"></i>
-                    <div>
-                        <span class="counter" data-count="99">0</span>
-                    </div>
-                    <h3>Kritik yang baik</h3>
-
-                </div>
-            </div>
-            <!-- end third count item -->
-        </div>
-        <!-- end row -->
-    </div>
-    <!-- end container -->
-</section>
-<!-- end section -->
-
-
 
 
 <!--Start Testimonial-->
@@ -250,8 +148,7 @@
                         <!-- /client info -->
                         <!-- client photo -->
                         <div class="client-thumb">
-                            <img src="{{ asset('assets/images/client-logo/clients-1.jpg') }}" class="img-fluid"
-                                alt="">
+                            <img src="{{ asset('assets/images/client-logo/clients-1.jpg') }}" class="img-fluid" alt="">
                         </div>
                         <div class="client-meta">
                             <h3>William Martin</h3>
@@ -274,8 +171,7 @@
                         <!-- /client info -->
                         <!-- client photo -->
                         <div class="client-thumb">
-                            <img src="{{ asset('assets/images/client-logo/clients-2.jpg') }}" class="img-fluid"
-                                alt="">
+                            <img src="{{ asset('assets/images/client-logo/clients-2.jpg') }}" class="img-fluid" alt="">
                         </div>
                         <div class="client-meta">
                             <h3>Emma Harrison</h3>
@@ -298,8 +194,7 @@
                         <!-- /client info -->
                         <!-- client photo -->
                         <div class="client-thumb">
-                            <img src="{{ asset('assets/images/client-logo/clients-3.jpg') }}" class="img-fluid"
-                                alt="">
+                            <img src="{{ asset('assets/images/client-logo/clients-3.jpg') }}" class="img-fluid" alt="">
                         </div>
                         <div class="client-meta">
                             <h3>Alexander Lucas</h3>
