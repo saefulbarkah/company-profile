6ce4a748fbb35fe8bc757231d07ea74fe4334b02
saefulbarkah <barkahgaming00102@gmail.com>
2021-09-02 10:42:54 +0700

    [Bingo - asset] menambahkan gambar portofolio

--- base-laravel/saeful barkah/bingo/resources/views/service/index.blade.php
+++ base-laravel/saeful barkah/bingo/resources/views/service/index.blade.php
@@ -8,7 +8,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center">
-                <h2>Pelayanan Kami</h2>
+                <h2></h2>
             </div>
         </div>
     </div>
