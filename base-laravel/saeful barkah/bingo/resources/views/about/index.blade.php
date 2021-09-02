68ba8518817088e7b70bc08b72f874fe42be6310
saefulbarkah <barkahgaming00102@gmail.com>
2021-09-02 08:38:19 +0700

    [Bingo - about] menghapus section

--- base-laravel/saeful barkah/bingo/resources/views/about/index.blade.php
+++ base-laravel/saeful barkah/bingo/resources/views/about/index.blade.php
@@ -156,21 +156,4 @@
     <!-- End container -->
 </section>
 <!-- End section -->
-
-<!-- Start Call To Action -->
-<section class="call-to-action section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <h2>Mari Menciptakan Sesuatu Bersama</h2>
-                <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicudin bibendum auctor, <br> nisi elit
-                    consequat ipsum, nesagittis sem nid elit. Duis sed odio sitain elit.</p>
-                <a href="" class="btn btn-main">Hubungi kami</a>
-            </div>
-        </div>
-        <!-- End row -->
-    </div>
-    <!-- End container -->
-</section>
-<!-- End section -->
 @endsection
