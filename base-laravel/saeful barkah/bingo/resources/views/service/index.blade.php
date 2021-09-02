9a1b7ce0fa2058f6db6957f06c055b481499143a
saefulbarkah <barkahgaming00102@gmail.com>
2021-09-02 09:10:01 +0700

    [Bingo - service] menambahkan isi layanan

--- base-laravel/saeful barkah/bingo/resources/views/service/index.blade.php
+++ base-laravel/saeful barkah/bingo/resources/views/service/index.blade.php
@@ -31,92 +31,85 @@
                 </div>
             </div>
             <!-- /section title -->
+        </div>
+        <!-- End row -->
 
+        <div class="row">
             <!-- Single Service Item -->
             <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0">
-                <div class="service-block p-4 color-bg text-center">
+                <div class="service-block p-4 text-center shadow">
                     <div class="service-icon text-center">
-                        <i class="tf-ion-ios-copy-outline"></i>
+                        <img src="{{ asset('assets/images/icon/order.png') }}" alt="" class="img-fluid" width="128px">
                     </div>
-                    <h3>TEMA WORDPRESS</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur.. Sed id lorem eget orci dictum facilisis vel id tellus.
-                        Nullam
-                        iaculis arcu at mauris dapibus consectetur.</p>
+                    <h3>Full Order Jersey</h3>
+                    <p></p>
                 </div>
             </div>
             <!-- End Single Service Item -->
 
             <!-- Single Service Item -->
             <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0">
-                <div class="service-block p-4 text-center">
+                <div class="service-block p-4 text-center shadow">
                     <div class="service-icon text-center">
-                        <i class="tf-ion-ios-alarm-outline"></i>
+                        <img src="{{ asset('assets/images/icon/shirt.png') }}" alt="" class="img-fluid" width="128px">
                     </div>
-                    <h3>DESAIN RESPONSIF</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur.. Sed id lorem eget orci dictum facilisis vel id tellus.
-                        Nullam
-                        iaculis arcu at mauris dapibus consectetur.</p>
+                    <h3>T-shirt Sablon</h3>
+                    <p></p>
                 </div>
             </div>
             <!-- End Single Service Item -->
 
             <!-- Single Service Item -->
             <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0">
-                <div class="service-block p-4 color-bg text-center">
+                <div class="service-block p-4 text-center shadow">
                     <div class="service-icon text-center">
-                        <i class="tf-ion-ios-book-outline"></i>
+                        <img src="{{ asset('assets/images/icon/kemeja.png') }}" alt="" class="img-fluid" width="128px">
                     </div>
-                    <h3>MEDIA &amp; IKLAN</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur.. Sed id lorem eget orci dictum facilisis vel id tellus.
-                        Nullam
-                        iaculis arcu at mauris dapibus consectetur.</p>
+                    <h3>Kemeja</h3>
+                    <p></p>
+                </div>
+            </div>
+            <!-- End Single Service Item -->
+        </div>
+        <div class="row mt-3 p-3">
+            <!-- Single Service Item -->
+            <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0">
+                <div class="service-block p-4 text-center shadow">
+                    <div class="service-icon text-center">
+                        <img src="{{ asset('assets/images/icon/hoodie.png') }}" alt="" class="img-fluid" width="128px">
+                    </div>
+                    <h3>Jaket</h3>
+                    <p></p>
                 </div>
             </div>
             <!-- End Single Service Item -->
 
             <!-- Single Service Item -->
             <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0">
-                <div class="service-block p-4  text-center">
+                <div class="service-block p-4 text-center shadow">
                     <div class="service-icon text-center">
-                        <i class="tf-ion-ios-briefcase-outline"></i>
+                        <img src="{{ asset('assets/images/icon/printing.png') }}" alt="" class="img-fluid"
+                            width="128px">
                     </div>
-                    <h3>DESAIN GRAFIS</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur.. Sed id lorem eget orci dictum facilisis vel id tellus.
-                        Nullam
-                        iaculis arcu at mauris dapibus consectetur.</p>
+                    <h3>Makloon Sublime Printing</h3>
+                    <p></p>
                 </div>
             </div>
             <!-- End Single Service Item -->
 
             <!-- Single Service Item -->
             <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0">
-                <div class="service-block p-4 color-bg text-center">
+                <div class="service-block p-4 text-center shadow">
                     <div class="service-icon text-center">
-                        <i class="tf-ion-ios-crop"></i>
+                        <img src="{{ asset('assets/images/icon/sublimation.png') }}" alt="" class="img-fluid"
+                            width="128px">
                     </div>
-                    <h3>Pengembangan Aplikasi</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur.. Sed id lorem eget orci dictum facilisis vel id tellus.
-                        Nullam
-                        iaculis arcu at mauris dapibus consectetur.</p>
+                    <h3>Makloon Press Sublime</h3>
+                    <p></p>
                 </div>
             </div>
             <!-- End Single Service Item -->
-
-            <!-- Single Service Item -->
-            <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0">
-                <div class="service-block p-4 text-center">
-                    <div class="service-icon text-center">
-                        <i class="tf-ion-ios-home-outline"></i>
-                    </div>
-                    <h3>Jaringan</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur.. Sed id lorem eget orci dictum facilisis vel id tellus.
-                        Nullam
-                        iaculis arcu at mauris dapibus consectetur.</p>
-                </div>
-            </div>
-            <!-- End Single Service Item -->
-
-        </div> <!-- End row -->
+        </div>
     </div> <!-- End container -->
 </section>
 <!-- End section -->
