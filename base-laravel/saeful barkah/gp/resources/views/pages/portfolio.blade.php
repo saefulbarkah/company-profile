f185f9296d106a6aa5fa7ccf411d0a76253e8741
saefulbarkah <barkahgaming00102@gmail.com>
2021-09-13 10:18:36 +0700

    [gp] revisi portfolio

--- base-laravel/saeful barkah/gp/resources/views/pages/portfolio.blade.php
+++ base-laravel/saeful barkah/gp/resources/views/pages/portfolio.blade.php
@@ -1,116 +1,114 @@
 @extends('layouts.master')
 
 @section('title', 'Genetic Home')
+@section('p-title', 'Portofolio')
 @section('content')
 <!-- ======= Portfolio Section ======= -->
 <section id="portfolio" class="portfolio">
-    <div class="container" data-aos="fade-up">
+    <div class="container">
 
         <div class="section-title">
-            <h2>Portofolio</h2>
-            <p>Portofolio kami</p>
+            <p>Portofolio</p>
         </div>
 
-        <div class="row container portfolio" data-aos="fade-up" data-aos-delay="100">
+        <div class="row container portfolio">
             <div class="col-lg-12 d-flex justify-content-right">
                 <ul id="portfolio-flters">
                     <li data-filter="*" class="filter-active">Semua</li>
-                    <li data-filter=".filter-hijau">Hijau</li>
-                    <li data-filter=".filter-merah">Merah</li>
-                    <li data-filter=".filter-orange">Oranye</li>
+                    <li data-filter=".filter-jersey">jersey</li>
+                    <li data-filter=".filter-jacket">Jacket</li>
+                    <li data-filter=".filter-kemeja">Kemeja</li>
+                    <li data-filter=".filter-tshirt">tshirt</li>
                 </ul>
             </div>
         </div>
 
-        <div class="row portfolio-container justify-content-start" data-aos="fade-up" data-aos-delay="200">
-
-            <div class="col-lg-4 col-md-6 portfolio-item filter-hijau">
+        <div class="row portfolio-container">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-jersey">
                 <div class="portfolio-wrap">
                     <img src="{{ asset('assets/img/portfolio/baju-hijau-depan.jpg') }}" class="img-fluid float-start"
                         alt="">
                     <div class="portfolio-info">
-                        <h4>T-shirt hijau Depan</h4>
-                        <p>Tshirt</p>
+                        <h4>Jersey Futsal</h4>
+
                         <div class="portfolio-links">
                             <a href="{{ asset('assets/img/portfolio/baju-hijau-depan.jpg') }}"
-                                data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i
-                                    class="bx bx-plus"></i></a>
-                            <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                                data-gallery="portfolioGallery" class="portfolio-lightbox" ><i
+                                    class="fas fa-search"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 portfolio-item filter-hijau">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-jersey">
                 <div class="portfolio-wrap">
                     <img src="{{ asset('assets/img/portfolio/baju-hijau-belakang.jpg') }}" class="img-fluid" alt="">
                     <div class="portfolio-info">
-                        <h4>T-shirt hijau belakang</h4>
-                        <p>Tshirt</p>
+                        <h4>Jersey Futsal</h4>
                         <div class="portfolio-links">
                             <a href="{{ asset('assets/img/portfolio/baju-hijau-belakang.jpg') }}"
-                                data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i
-                                    class="bx bx-plus"></i></a>
-                            <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                                data-gallery="portfolioGallery" class="portfolio-lightbox" ><i
+                                    class="fas fa-search"></i></a>
+
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 portfolio-item filter-merah">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-jersey">
                 <div class="portfolio-wrap">
                     <img src="{{ asset('assets/img/portfolio/baju-merah-depan.jpg') }}" class="img-fluid" alt="">
                     <div class="portfolio-info">
-                        <h4>T-shirt merah belakang</h4>
-                        <p>Tshirt</p>
+                        <h4>Jersey Futsal</h4>
+
                         <div class="portfolio-links">
                             <a href="{{ asset('assets/img/portfolio/baju-merah-depan.jpg') }}"
-                                data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i
-                                    class="bx bx-plus"></i></a>
-                            <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                                data-gallery="portfolioGallery" class="portfolio-lightbox" ><i
+                                    class="fas fa-search"></i></a>
+
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 portfolio-item filter-merah">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-jersey">
                 <div class="portfolio-wrap">
                     <img src="{{ asset('assets/img/portfolio/baju-merah-belakang.jpg') }}" class="img-fluid" alt="">
                     <div class="portfolio-info">
-                        <h4>T-shirt merah belakang</h4>
-                        <p>Tshirt</p>
+                        <h4>Jersey Futsal</h4>
+
                         <div class="portfolio-links">
                             <a href="{{ asset('assets/img/portfolio/baju-merah-belakang.jpg') }}"
-                                data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i
-                                    class="bx bx-plus"></i></a>
-                            <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                                data-gallery="portfolioGallery" class="portfolio-lightbox" ><i
+                                    class="fas fa-search"></i></a>
+
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 portfolio-item filter-orange">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-jersey">
                 <div class="portfolio-wrap">
                     <img src="{{ asset('assets/img/portfolio/baju-orange-belakang.jpg') }}" class="img-fluid" alt="">
                     <div class="portfolio-info">
-                        <h4>T-shirt oranye belakang</h4>
-                        <p>Tshirt</p>
+                        <h4>Jersey Futsal</h4>
+
                         <div class="portfolio-links">
                             <a href="{{ asset('assets/img/portfolio/baju-orange-belakang.jpg') }}"
-                                data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i
-                                    class="bx bx-plus"></i></a>
-                            <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                                data-gallery="portfolioGallery" class="portfolio-lightbox" ><i
+                                    class="fas fa-search"></i></a>
+
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 portfolio-item filter-orange float-start">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-jersey float-start">
                 <div class="portfolio-wrap">
                     <img src="{{ asset('assets/img/portfolio/baju-orange-depan.jpg') }}" class="img-fluid" alt="">
                     <div class="portfolio-info">
-                        <h4>T-shirt oranye depan</h4>
-                        <p>Tshirt</p>
+                        <h4>Jersey Futsal</h4>
+
                         <div class="portfolio-links">
                             <a href="{{ asset('assets/img/portfolio/baju-orange-depan.jpg') }}"
-                                data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i
-                                    class="bx bx-plus"></i></a>
-                            <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                                data-gallery="portfolioGallery" class="portfolio-lightbox" ><i
+                                    class="fas fa-search"></i></a>
+
                         </div>
                     </div>
                 </div>
