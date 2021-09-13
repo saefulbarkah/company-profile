6042970ff7fb5c0c1585d9dfa5dcc9e841329919
saefulbarkah <barkahgaming00102@gmail.com>
2021-09-13 09:30:14 +0700

    [gp] mengubah bagian tim

--- base-laravel/saeful barkah/gp/resources/views/pages/about.blade.php
+++ base-laravel/saeful barkah/gp/resources/views/pages/about.blade.php
@@ -14,7 +14,7 @@
             <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
                 <img src="assets/img/about.jpg" class="img-fluid" alt="">
             </div>
-            <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+            <div class="col-lg-12 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
                 <h3>Genetic Wear</h3>
                 <p>
                     Genetic Wear merupakan sebuah perusahaan konveksi yang memiliki layanan Design, pembuatan
@@ -62,40 +62,23 @@
         </div>
 
         <div class="row justify-content-center">
-
-            <div class="col-lg-4 col-md-6 d-flex align-items-stretch text-center">
-                <div class="member" data-aos="fade-up" data-aos-delay="100">
-                    <div class="member-img">
-                        <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                            <a href=""><i class="bi bi-twitter"></i></a>
-                            <a href=""><i class="bi bi-facebook"></i></a>
-                            <a href=""><i class="bi bi-instagram"></i></a>
-                            <a href=""><i class="bi bi-linkedin"></i></a>
-                        </div>
+            <div class="col-lg-8">
+                <div class="card-group">
+                    <div class="card text-center mx-3 shadow">
+                      <img src="{{ asset('assets/img/team/hannif.jpg') }}" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Hannif Kurniawan</h5>
+                        <p class="card-text">CEO</p>
+                      </div>
                     </div>
-                    <div class="member-info">
-                        <h4>Hannif Kurniawan</h4>
-                        <span>CEO</span>
+                    <div class="card text-center mx-3 shadow">
+                      <img src="{{ asset('assets/img/team/angga.jpg') }}" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Angga Pratama</h5>
+                        <p class="card-text">COO</p>
+                      </div>
                     </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 d-flex align-items-stretch text-center">
-                <div class="member" data-aos="fade-up" data-aos-delay="100">
-                    <div class="member-img">
-                        <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                        <div class="social">
-                            <a href=""><i class="bi bi-twitter"></i></a>
-                            <a href=""><i class="bi bi-facebook"></i></a>
-                            <a href=""><i class="bi bi-instagram"></i></a>
-                            <a href=""><i class="bi bi-linkedin"></i></a>
-                        </div>
-                    </div>
-                    <div class="member-info">
-                        <h4>Angga Pratama</h4>
-                        <span>COO</span>
-                    </div>
-                </div>
+                  </div>
             </div>
         </div>
 
