e22f7c76d14fe2cd07db11966787b899da559214
saefulbarkah <barkahgaming00102@gmail.com>
2021-09-14 08:51:28 +0700

    [gp] fix

--- base-laravel/saeful barkah/gp/resources/views/pages/service.blade.php
+++ base-laravel/saeful barkah/gp/resources/views/pages/service.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.master')
 
 @section('title', 'Genetic | Layanan')
-@section('p-title', 'Layanan')
 @section('content')
 
 <!-- ======= Services Section ======= -->
 <section id="services" class="services">
     <div class="container" data-aos="fade-up">
 
-        <div class="section-title">
+        <div class="section-title text-center">
             <p>Layanan</p>
         </div>
 
@@ -19,7 +18,7 @@
                     <h4><a href="{{ url('layanan/full-order') }}">Full Order</a></h4>
                     <p>Full order merupakan layanan produk berupa pembelian penuh dengan kelengkapan yang sesuai layanan</p>
                     <div class="mt-4">
-                        <a href="{{ url('layanan/full-order') }}" class="btn btn-dark text-warning">Lihat selengkapnya</a>
+                        <a href="{{ url('layanan/full-order') }}" class="btn btn-dark text-warning">Selengkapnya</a>
                     </div>
                 </div>
             </div>
@@ -27,11 +26,11 @@
             <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in"
                 data-aos-delay="200">
                 <div class="icon-box">
-                    <div class="icon"><i class="fas fa-tshirt"></i></div>
+                    <div class="icon"><i class="fas fa-print"></i></div>
                     <h4><a href="{{ url('layanan/makloon-sublime') }}">Makloon Sublime</a></h4>
                     <p>Makloon sublime merupakan layanan jasa yang menyediakan layanan berupa printing & press heat transfer</p>
                     <div class="mt-4">
-                        <a href="{{ url('layanan/makloon-sublime') }}" class="btn btn-dark text-warning">Lihat selengkapnya</a>
+                        <a href="{{ url('layanan/makloon-sublime') }}" class="btn btn-dark text-warning">Selengkapnya</a>
                     </div>
                 </div>
             </div>
