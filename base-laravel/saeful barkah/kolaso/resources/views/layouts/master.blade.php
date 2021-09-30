0b125587cf85d83090a16e0a6a4547a5cf5eb368
saefulbarkah <saefulbarkah29062003@gmail.com>
2021-09-30 09:09:39 +0700

    [kolaso] console fix

--- base-laravel/saeful barkah/kolaso/resources/views/layouts/master.blade.php
+++ base-laravel/saeful barkah/kolaso/resources/views/layouts/master.blade.php
@@ -13,8 +13,6 @@
     <meta name="author" content="Marko Smith" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="description" content="Multi-purpose Business html5 template">
-    <link href="{{ asset('assets/images/favicon/favicon.png' ) }}" rel="icon">
-
     <link
         href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i%7CPoppins:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
